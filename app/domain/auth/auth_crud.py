9377b1480b0d32c7fc25c104fa37bff33ff868c1
unknown <isakin.tyrfing@gmail.com>
2024-07-12 17:42:14 +0900

    [Refactor] replace user_schema to auth_schema

--- app/domain/auth/auth_crud.py
+++ app/domain/auth/auth_crud.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 
 from ...models import user_model
-from ...schemas import user_schema
+from . import auth_schema
 
 
-def create_user(db: Session, user: user_schema.UserCreate):
+def create_user(db: Session, user: auth_schema.UserCreate):
     db_user = user_model.User(
         email=user.email,
         hashed_password=user.hashed_password,
