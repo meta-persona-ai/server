6cb195ee987e79180a56c94d0b4eb683ff083ff5
MinkyoDev <isakin.tyrfing@gmail.com>
2024-07-20 16:10:17 +0900

    [Chore]: remake swagger config

--- app/core/swagger_config.py
+++ app/core/swagger_config.py
@@ -1,27 +1,41 @@
-title="Persona AI"
+import textwrap
 
-version="0.0.1"
+class SwaggerConfig:
+    def __init__(self):
+        self.title = "Persona AI"
+        self.version = "0.0.1"
+        self.description = textwrap.dedent("""\
+            #### 페르소나 AI
 
-description = """
-#### 페르소나 AI
+            기능 목록:
 
-기능 목록:
+            * **auth** (_not implemented_).
+            * **user** (_not implemented_).
+        """)
+        self.license_info = {
+            "name": "Apache 2.0",
+            "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        }
+        self.tags_metadata = [
+            {
+                "name": "Auth",
+                "description": "로그인과 권한 관련 API입니다.",
+            },
+            {
+                "name": "User",
+                "description": "유저 관련 API입니다."
+            },
+            {
+                "name": "Chat",
+                "description": "채팅방 관련 API입니다."
+            },
+        ]
 
-* **auth** (_not implemented_).
-* **user** (_not implemented_).
-"""
-
-tags_metadata = [
-    {
-        "name": "Auth",
-        "description": "로그인과 권한 관련 API입니다.",
-    },
-    {
-        "name": "User",
-        "description": "유저 관련 API입니다."
-    },
-    {
-        "name": "Chat",
-        "description": "채팅방 관련 API입니다."
-    },
-]
+    def get_config(self):
+        return {
+            "title": self.title,
+            "version": self.version,
+            "description": self.description,
+            "license_info": self.license_info,
+            "tags_metadata": self.tags_metadata,
+        }
