2fed0444e01e39067b32bfe594ed7a57531ce0c0
unknown <isakin.tyrfing@gmail.com>
2024-08-05 09:24:49 +0900

    [Refactor]: add chatting end message

--- app/services/chatting_service.py
+++ app/services/chatting_service.py
@@ -33,6 +33,7 @@ async def chatting(websocket: WebSocket, chat_id: int, db: Session):
     await room.connect(websocket)
     logger.info(f"📌 WebSocket connection established with room: {chat_id}")
 
+    # Validate Chatting
     try:
         user: User = await authenticate_user(websocket, db)
         chat = await validate_chat_room(chat_id, user, db)
@@ -41,10 +42,12 @@ async def chatting(websocket: WebSocket, chat_id: int, db: Session):
         await websocket.close(code=1008, reason=e.detail)
         return
     
+    # Chatting Start
     logger.info(f"📌 User {user.user_name}({user.user_id}) joined the room {chat_id}")
     await room.broadcast_system_message(f"A new client has joined {chat_id}.")
     
     try:
+        # Chatting Handler
         await handle_messages(websocket, room, chat, db)
     except WebSocketDisconnect:
         await handle_disconnect(websocket, room_manager, room, chat_id, user)
@@ -63,19 +66,25 @@ async def handle_messages(websocket: WebSocket, room: ConnectionManager, chat: C
         character_schema (CharacterSchema): 캐릭터 스키마 객체.
         db (Session): 데이터베이스 세션 객체.
     """
+    # Make Initial Data
     user_info, character_info, chat_history = data_converter(chat)
 
+    # Make Gemini Chain
     gemini = Gemini(
         user_info = user_info,
         character_info = character_info, 
         chat_history = chat_history
     )
 
+    # Socket Listener
     while True:
+        # Recieve Message
         data = await websocket.receive_text()
         user_message = UserMessage(**json.loads(data))
 
         response_id = room.get_next_response_id()
+
+        # Create Answer
         asyncio.create_task(echo_message(room, gemini,chat, user_message, response_id, db=db))
 
 def data_converter(chat: Chat) -> tuple[dict[str, object], dict[str, object], list[dict[str, str]]]:
